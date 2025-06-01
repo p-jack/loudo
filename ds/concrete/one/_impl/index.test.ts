158b68c7b7ffcf27592da7aeea22aedcab6fc6f2
Paul Jack <134734881+p-jack@users.noreply.github.com>
2025-06-01 14:18:22 -0700

    ds/concrete/one 0.0.14

---
+++ ds/concrete/one/_impl/index.test.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "vitest"
+import { One } from "./index"
+
+test("One", () => {
+  const one = One.of("111")
+  expect(one.size).toBe(1)
+  expect(one.empty).toBe(false)
+  expect(one.has("111")).toBe(true)
+  expect(one.has("222")).toBe(false)
+  expect(one.first).toBe("111")
+  expect(one.last).toBe("111")
+  expect(one.only).toBe("111")
+  expect([...one]).toStrictEqual(["111"])
+  expect([...one]).toStrictEqual(["111"])
+})
---
+++ ds/concrete/one/_impl/index.ts
@@ -0,0 +1,28 @@
+import { Sized } from "loudo-ds-core"
+import { mixin } from "loudo-mixin"
+
+const value = Symbol("value")
+const eq = Symbol("eq")
+
+export class One<T extends {}> {
+
+  protected [value]:T
+  protected [eq]:(a:T,b:T)=>boolean
+
+  private constructor(v:T, _eq:(a:T,b:T)=>boolean) {
+    this[value] = v
+    this[eq] = _eq
+  }
+
+  get size() { return 1 }
+  get first() { return this[value] }
+  get last() { return this[value] }
+  get only() { return this[value] }
+  get eq() { return this[eq] }
+  *[Symbol.iterator]() { yield this[value] }
+
+  static of<T extends {}>(v:T, eq:(a:T,b:T)=>boolean = Object.is) { return new One(v, eq) }
+
+}
+export interface One<T extends {}> extends Sized<T> {}
+mixin(One, [Sized])
