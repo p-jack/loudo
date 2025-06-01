23ead48e979d9f8873cb14c4813eba5799db0fb1
Paul Jack <134734881+p-jack@users.noreply.github.com>
2025-05-31 18:49:11 -0700

    ds/tools/capture 0.0.5

---
+++ ds/tools/capture/index.test.ts
@@ -0,0 +1,75 @@
+import { Loud } from "loudo-ds-core"
+import { mixin } from "loudo-mixin"
+import { expect, test } from "vitest"
+import { capture } from "./index"
+
+class Empty<T extends {}> {
+  *[Symbol.iterator]():Iterator<T> { }
+}
+interface Empty<T extends {}> extends Loud<T> {}
+mixin(Empty, [Loud])
+
+class Foo<T extends {}> {
+  constructor(private _value:T) {}
+  *[Symbol.iterator]() { yield this._value }
+  get value() { return this._value }
+  set value(v:T) {
+    const old = this._value
+    this._value = v
+    this.fire({
+      removed:{ elements:new Foo(old), at:0 },
+      added:{ elements:this, at:0 },
+    })
+  }
+  clear() { 
+    this.fire({ cleared: 1 })
+  }
+  bad() { this.fire({
+    added:{ elements: new Empty<T>(), at:-1 },
+  })}
+  good() {
+    this.fire({
+      added:{ elements:this, at:-1 }
+    })
+  }
+}
+interface Foo<T extends {}> extends Loud<T> {}
+mixin(Foo, [Loud])
+
+test("capture", () => {
+  const foo = new Foo("1")
+  const c = capture(foo)
+  expect(c.get()).toStrictEqual({
+    added: { elements:["1"], at:0 },
+  })
+  expect(c.get()).toBeUndefined()
+  foo.value = "2"
+  expect(c.get()).toStrictEqual({
+    removed: { elements:["1"], at:0 },
+    added: { elements:["2"], at:0 },
+  })
+  expect(c.get()).toBeUndefined()
+  foo.bad()
+  expect(() => { c.get() }).toThrowError()
+  expect(c.get()).toBeUndefined()
+  foo.clear()
+  expect(c.get()).toStrictEqual({ cleared: 1 })
+  foo.good()
+  expect(c.get()).toStrictEqual({added:{elements:["2"]}})
+})
+
+test("no index", () => {
+  const foo = new Foo("1")
+  const c = capture(foo)
+  expect(c.get()).toStrictEqual({
+    added: { at:0, elements:["1"] },
+  })
+})
+
+test("entries", () => {
+  const foo = new Foo({ key:"1", value:1, extra:"ignored" })
+  const c = capture(foo)
+  expect(c.get()).toStrictEqual({
+    added: { elements:[{ key:"1", value:1 }] },
+  })
+})
---
+++ ds/tools/capture/index.ts
@@ -0,0 +1,54 @@
+import { LEvent, Mod as ModI, Loud } from "loudo-ds-core"
+
+export interface Mod<T extends {}> {
+  elements:T[]
+  at?:number
+}
+
+export interface Evt<T extends {}> {
+  cleared?: number
+  added?: Mod<T>
+  removed?: Mod<T>
+}
+
+export interface Capture<T extends {}> {
+  get():Evt<T>|undefined
+}
+
+function mod<T extends {}>(mod?:ModI<T>):Mod<T>|undefined {
+  if (mod === undefined) return undefined
+  const at = mod.at
+  const elements = [...mod.elements]
+  if (elements.length === 0) throw new TypeError("mod should have elements")
+  const first = elements[0]!
+  if (typeof(first) === "object" && "key" in first && "value" in first) {
+    return { elements:elements.map((x:any) => { return { key:x.key, value:x.value }}) as never}
+  }
+  if (at < 0) return { elements }
+  return { at, elements }
+}
+
+function evt<T extends {}>(event?:LEvent<T>):Evt<T>|undefined {
+  if (event === undefined) return undefined
+  const e:Evt<T> = {  }
+  if (event.cleared ?? 0 > 0) e.cleared = event.cleared
+  const a = mod(event.added)
+  if (a) e.added = a
+  const r = mod(event.removed)
+  if (r) e.removed = r
+  return e
+}
+
+export function capture<T extends {}>(stash:Loud<T>):Capture<T> {
+  let captured:LEvent<T>|undefined = undefined
+  stash.hear(globalThis, event => {
+    captured = event
+  })
+  return {
+    get:() => {
+      const r = captured;
+      captured = undefined;
+      return evt(r)
+    },
+  }
+}
