d34815a210698c65d099bd8fce7b00de0e3e27e3
Paul Jack <134734881+p-jack@users.noreply.github.com>
2025-06-09 17:31:51 -0700

    loudo-ds-doubly 0.0.1

---
+++ ds/concrete/doubly/_impl/index.test.ts
@@ -0,0 +1,146 @@
+import { beforeEach, expect, test } from "vitest";
+import { Doubly } from "./index";
+import { capture } from "loudo-ds-tools-capture";
+
+let d = new Doubly(["A"])
+let c = capture(d)
+let r = d.reversed()
+let e = d.toEmpty()
+
+beforeEach(() => {
+  d = new Doubly(["A", "B", "C"])
+  r = d.reversed()
+  c = capture(d)
+  e = d.toEmpty()
+})
+
+test("add", () => {
+  d.add("D")
+  expect([...d]).toStrictEqual(["A", "B", "C", "D"])
+  expect([...r]).toStrictEqual(["D", "C", "B", "A"])
+  expect(d.size).toBe(4)
+  expect(c.get()).toStrictEqual({added:{elements:["D"], at:3}})
+  e.push("X")
+  expect(e.size).toBe(1)
+  expect([...e]).toStrictEqual(["X"])
+})
+
+test("clear", () => {
+  d.clear()
+  expect([...d]).toStrictEqual([])
+  expect([...r]).toStrictEqual([])
+  expect(d.size).toBe(0)
+  expect(c.get()).toStrictEqual({cleared:3})
+})
+
+test("first", () => {
+  expect(d.first).toBe("A")
+  expect(e.first).toBeUndefined()
+})
+
+test("iterator", () => {
+  expect([...d]).toStrictEqual(["A", "B", "C"])
+  expect([...e]).toStrictEqual([])
+})
+
+test("last", () => {
+  expect(d.last).toBe("C")
+  expect(e.last).toBeUndefined()
+})
+
+test("of", () => {
+  const d = Doubly.of("1", "2", "3")
+  expect([...d]).toStrictEqual(["1", "2", "3"])
+})
+
+test("only", () => {
+  expect(() => d.only).toThrowError()
+  expect(() => e.only).toThrowError()
+  e.push("Z")
+  expect(e.only).toBe("Z")
+})
+
+test("pop", () => {
+  expect(d.pop()).toBe("C")
+  expect(d.size).toBe(2)
+  expect([...d]).toStrictEqual(["A", "B"])
+  expect([...r]).toStrictEqual(["B", "A"])
+  expect(c.get()).toStrictEqual({removed:{elements:["C"], at:2}})
+  expect(d.pop()).toBe("B")
+  expect(d.size).toBe(1)
+  expect([...d]).toStrictEqual(["A"])
+  expect([...r]).toStrictEqual(["A"])
+  expect(c.get()).toStrictEqual({removed:{elements:["B"], at:1}})
+  expect(d.pop()).toBe(("A"))
+  expect([...d]).toStrictEqual([])
+  expect([...r]).toStrictEqual([])
+  expect(d.size).toBe(0)
+  expect(c.get()).toStrictEqual({removed:{elements:["A"], at:0}})
+  expect(d.pop()).toBeUndefined()
+})
+
+test("push", () => {
+  d.push("D")
+  expect([...d]).toStrictEqual(["A", "B", "C", "D"])
+  expect([...r]).toStrictEqual(["D", "C", "B", "A"])
+  expect(d.size).toBe(4)
+  expect(c.get()).toStrictEqual({added:{elements:["D"], at:3}})
+  e.push("X")
+  expect(e.size).toBe(1)
+  expect([...e]).toStrictEqual(["X"])
+})
+
+test("replace", () => {
+  d.replace([])
+  expect(c.get()).toStrictEqual({cleared:3})
+  d.replace([])
+  expect(c.get()).toBeUndefined()
+  d.replace(["1", "2", "3"])
+  expect(c.get()).toStrictEqual({added:{elements:["1", "2", "3"], at:0}})
+  d.replace(["A", "B", "C"])
+  expect(c.get()).toStrictEqual({cleared:3, added:{elements:["A", "B", "C"], at:0}})
+})
+
+test("reversed", () => {
+  expect([...r]).toStrictEqual(["C", "B", "A"])
+  expect([...r]).toStrictEqual(["C", "B", "A"])
+  expect(r.size).toBe(3)
+})
+
+test("shift", () => {
+  expect(d.shift()).toBe("A")
+  expect([...d]).toStrictEqual(["B", "C"])
+  expect([...r]).toStrictEqual(["C", "B"])
+  expect(d.size).toBe(2)
+  expect(r.size).toBe(2)
+  expect(c.get()).toStrictEqual({removed:{elements:["A"], at:0}})
+  expect(d.shift()).toBe("B")
+  expect([...d]).toStrictEqual(["C"])
+  expect([...r]).toStrictEqual(["C"])
+  expect(d.size).toBe(1)
+  expect(r.size).toBe(1)
+  expect(c.get()).toStrictEqual({removed:{elements:["B"], at:0}})
+  expect(d.shift()).toBe("C")
+  expect([...d]).toStrictEqual([])
+  expect([...r]).toStrictEqual([])
+  expect(d.size).toBe(0)
+  expect(r.size).toBe(0)
+  expect(c.get()).toStrictEqual({removed:{elements:["C"], at:0}})
+  expect(d.shift()).toBeUndefined()
+})
+
+test("size", () => {
+  expect(d.size).toBe(3)
+  expect(e.size).toBe(0)
+})
+
+test("unshift", () => {
+  d.unshift("0")
+  expect([...d]).toStrictEqual(["0", "A", "B", "C"])
+  expect([...r]).toStrictEqual(["C", "B", "A", "0"])
+  expect(d.size).toBe(4)
+  expect(c.get()).toStrictEqual({added:{elements:["0"], at:0}})
+  e.unshift("a")
+  expect([...e]).toStrictEqual(["a"])
+  expect(e.size).toBe(1)
+})
---
+++ ds/concrete/doubly/_impl/index.ts
@@ -0,0 +1,161 @@
+import { OnlyError, Parsable, Sized, sized } from "loudo-ds-core";
+import { mixin } from "loudo-mixin";
+import { One } from "loudo-ds-one";
+
+
+interface Node<T extends {}> {
+  value: T
+  next?: Node<T>
+  prev?: Node<T>
+}
+
+const first = Symbol("first")
+const last = Symbol("last")
+const size = Symbol("size")
+const conf = Symbol("conf")
+const reversed = Symbol("reversed")
+
+export interface Conf<T extends {}> {
+  eq:(a:T,b:T)=>boolean
+}
+
+export class Doubly<T extends {}> {
+
+  protected [first]:Node<T>|undefined
+  protected [last]:Node<T>|undefined
+  protected [size]:number
+  protected [conf]:Conf<T>
+
+  constructor(elements:Iterable<T>, config:Conf<T> = { eq:Object.is }) {
+    this[size] = 0
+    this[conf] = config
+    for (const x of elements) this.push(x)
+  }
+
+  static of<T extends {}>(...elements:T[]) {
+    return new Doubly(elements)
+  }
+
+  get size() { return this[size] }
+  get eq() { return this[conf].eq }
+  get first() { return this[first]?.value }
+  get last() { return this[last]?.value }
+  get only() {
+    if (this.size !== 1) throw new OnlyError()
+    return (this[first] as Node<T>).value
+  }
+
+  *[Symbol.iterator]() {
+    for (let n = this[first]; n !== undefined; n = n.next) yield n.value
+  }
+
+  private *[reversed]() {
+    for (let n = this[last]; n !== undefined; n = n.prev) yield n.value
+  }
+
+  reversed() {
+    const me = this
+    return sized(() => me[reversed](), () => me.size, me.eq)
+  }
+
+  protected rawAdd(value:T) {
+    const n:Node<T> = { value }
+    const sz = this[size]
+    const l = this[last]
+    if (l === undefined) {
+      this[last] = n
+      this[first] = n
+    } else {
+      l.next = n
+      n.prev = l
+      this[last] = n
+    }
+    this[size]++
+  }
+
+  add(value:T) {
+    this.push(value)
+  }
+
+  push(value:T) {
+    const sz = this[size]
+    this.rawAdd(value)
+    this.fire({added:{elements:One.of(value), at:sz }})
+  }
+
+  pop() {
+    const l = this[last]
+    if (l === undefined) return undefined
+    if (l.prev === undefined) this[first] = undefined
+    else l.prev.next = undefined
+    this[last] = l.prev
+    this[size]--
+    const sz = this[size]
+    this.fire({removed:{elements:One.of(l.value), at:sz }})
+    return l.value
+  }
+
+  protected rawUnshift(value:T) {
+    const n:Node<T> = { value }
+    const f = this[first]
+    if (f === undefined) {
+      this[first] = n
+      this[last] = n
+    } else {
+      n.next = f
+      f.prev = n
+      this[first] = n
+    }
+    this[size]++
+  }
+
+  unshift(value:T) {
+    this.rawUnshift(value)
+    this.fire({added:{elements:One.of(value), at:0}})
+  }
+
+  shift() {
+    const f = this[first]
+    if (f === undefined) return undefined
+    if (f.next === undefined) this[last] = undefined
+    else f.next.prev = undefined
+    this[first] = f.next
+    this[size]--
+    this.fire({removed:{elements:One.of(f.value), at:0}})
+    return f.value
+  }
+
+  clear() {
+    const cleared = this[size]
+    this[first] = undefined
+    this[last] = undefined
+    this[size] = 0
+    this.fire({cleared})
+  }
+
+  replace(i:Iterable<T>) {
+    const cleared = this[size]
+    this[first] = undefined
+    this[last] = undefined
+    this[size] = 0
+    let c = 0
+    for (const x of i) {
+      this.rawAdd(x)
+      c++
+    }
+    if (c === 0) {
+      if (cleared === 0) return
+      this.fire({cleared})
+      return
+    }
+    const elements = sized(() => i[Symbol.iterator](), () => c, this.eq)
+    const added = { elements, at:0 }
+    if (cleared > 0) this.fire({cleared:cleared, added})
+    else this.fire({added})
+  }
+
+  toEmpty():Doubly<T> { return new Doubly<T>([], this[conf]) }
+
+}
+export interface Doubly<T> extends Parsable<T> {}
+mixin(Doubly, [Parsable])
