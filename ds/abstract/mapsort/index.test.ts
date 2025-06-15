347d10d985d2edcbec7da8a9c8da08ae6f03b860
Paul Jack <134734881+p-jack@users.noreply.github.com>
2025-06-14 17:11:16 -0700

    loudo-ds-mapsort-interfaces 0.0.15

--- ds/abstract/mapsort/index.test.ts
+++ ds/abstract/mapsort/index.test.ts
@@ -1,16 +1,18 @@
 import { test, expect } from "vitest"
-import { MapsortBase } from "./index"
+import { MapsortBase, MapsortChange } from "./index"
 import { Loud, Sized, Stash } from "loudo-ds-core"
-import { MapBase } from "loudo-ds-map-interfaces"
+import { MapBase, MapChange } from "loudo-ds-map-interfaces"
 import { mixin, mixed } from "loudo-mixin"
 
 class X {}
-interface X extends MapsortBase<number,string> {}
+interface X extends MapsortChange<number,string> {}
 mixin(X, [MapsortBase])
 
 test("mapsorts", () => {
   const m = new X()
+  expect(mixed(m, MapsortChange)).not.toBeUndefined()
   expect(mixed(m, MapsortBase)).not.toBeUndefined()
+  expect(mixed(m, MapChange)).not.toBeUndefined()
   expect(mixed(m, MapBase)).not.toBeUndefined()
   expect(mixed(m, Loud)).not.toBeUndefined()
   expect(mixed(m, Sized)).not.toBeUndefined()
--- ds/abstract/mapsort/index.ts
+++ ds/abstract/mapsort/index.ts
@@ -1,13 +1,13 @@
 import { Sized, Include, Stash, Loud } from "loudo-ds-core"
 import { mixin } from "loudo-mixin"
-import { Entry, MapBase } from "loudo-ds-map-interfaces"
+import { Entry, MapBase, MapChange } from "loudo-ds-map-interfaces"
 
 export abstract class MapsortBase<K extends {},V extends {}> {
   abstract get unique():boolean
   abstract get compare():(a:K,b:K)=>number
-  abstract get last():V|undefined
+  abstract get last():Entry<K,V>|undefined
   abstract reversed():Sized<Entry<K,V>>
-  abstract range(start:K, end:K, include?:Include):Stash<V>
+  abstract range(start:K, end:K, include?:Include):Stash<Entry<K,V>>
   abstract before(v:K):V|undefined
   abstract after(v:K):V|undefined
   abstract from(v:K):V|undefined
@@ -19,5 +19,5 @@ mixin(MapsortBase, [MapBase])
 export abstract class MapsortChange<K extends {},V extends {}> {
   abstract set compare(cmp:(a:K,b:K)=>number)
 }
-export interface MapsortChange<K extends {},V extends {}> extends MapsortBase<K,V>, Loud<Entry<K,V>> {}
+export interface MapsortChange<K extends {},V extends {}> extends MapsortBase<K,V>, MapChange<K,V> {}
 mixin(MapsortChange, [MapsortBase, Loud])
